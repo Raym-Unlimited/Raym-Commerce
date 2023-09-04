d3703909cd4b36c6ebda8b3ee5fc34472d795207
RaymUnlimited <121440452+RaymUnlimited@users.noreply.github.com>
2023-09-04 01:17:14 +0100

    Delete store_app directory

--- store_app/lib/data/model/response/running_order_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:sixam_mart_store/data/model/response/order_model.dart';
-import 'package:flutter/material.dart';
-
-class RunningOrderModel {
-  String status;
-  List<OrderModel> orderList;
-
-  RunningOrderModel({@required this.status, @required this.orderList});
-}
